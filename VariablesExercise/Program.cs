6104ce3de29d8350f382169e6e29be95a3b6f522
felix adnel <flixadnel@gmail.com>
2023-06-22 02:54:47 -0500

    fixed and updated

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace VariablesExercise
+﻿namespace Variables
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Chachy";
+            int age = 22;
+            char middleInitial = 'a';
+            bool isOver18YearsOld = true;
+            double currentTemp = 60;
+            decimal currentPrice = 77.07m;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Is Over 18: {isOver18YearsOld}");
+            Console.WriteLine($"Current Temperature: {currentTemp}");
+            Console.WriteLine($"Current Price: {currentPrice}");
         }
     }
 }
